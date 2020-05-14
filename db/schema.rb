e35a2ab03a02256e38945374e79b734c79e93cfd
yamahiro1994 <y3h5.ja@gmail.com>
2020-05-14 11:59:38 +0900

    user関連修正
    email column 追加
    index username unique 削除
    index email unique 追加
    sign in view 修正
    alert margin-bottom 対処
    user registation,new email 追加
    user registation,edit email
    password 紛失編集
    TopPage header 編集

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_12_055515) do
+ActiveRecord::Schema.define(version: 2020_05_14_003236) do
 
   create_table "events", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 2020_05_12_055515) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "events", "users"
